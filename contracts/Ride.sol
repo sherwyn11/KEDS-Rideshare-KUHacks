f76f8cb1e2d473f7146a606bd34defdbe786b9be
Sherwyn Dsouza <sherwyndsouza1999@gmail.com>
2021-02-20 03:12:06 +0530

    :rocket: added smart contracts v1.0

---
+++ contracts/Ride.sol
@@ -0,0 +1,52 @@
+pragma solidity ^0.6.6;
+pragma experimental ABIEncoderV2;
+
+contract Ride {
+
+    address riderAddr;
+    address driverAddr;
+    string[] fromAddr;
+    string[] toAddr;
+    bytes32 amount;
+    bool complete;
+    bool confirmedByRider;
+    bool confirmedByDriver;
+    
+    event UpdateConfirmationEvent(address _driverAddr);
+
+    
+    constructor(address _riderAddr, address _driverAddr, string[] memory _fromAddr, string[] memory _toAddr, bytes32 _amount) public {
+        riderAddr = _riderAddr;
+        driverAddr = _driverAddr;
+        fromAddr = _fromAddr;
+        toAddr = _toAddr;
+        amount = _amount;
+        complete = false;
+        confirmedByRider = false;
+        confirmedByDriver = false;
+    }
+    
+    function getRideInfo() view public returns(address, address, string[] memory, string[] memory, bytes32, bool, bool, bool) {
+        return (riderAddr, driverAddr, fromAddr, toAddr, amount, complete, confirmedByRider, confirmedByDriver);
+    }
+    
+    function updateDriverAddress(address payable _driverAddr) public {
+        driverAddr = _driverAddr;
+        emit UpdateConfirmationEvent(_driverAddr);
+    }
+    
+    function updateRiderConfirmation(bool _status) public {
+        require(confirmedByRider != _status, 'Some error occurred!');
+        confirmedByRider = _status;
+    }
+    
+    function updateDriverConfirmation(bool _status) public {
+        require(confirmedByDriver != _status, 'Some error occurred!');
+        confirmedByDriver = _status;
+    }
+    
+    function updateRideComplete(bool _complete) public {
+        require(complete != _complete, 'Some error occurred!');
+        complete = _complete;
+    }
+}
