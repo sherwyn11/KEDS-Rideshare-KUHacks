d9f57a5a591c0d7857d756c4255fc29b9a942413
Kevlyn Kadamala <kevlyn@gmail.com>
2021-02-20 20:00:48 +0530

    Saving the source destination time and duration

--- client/src/App.js
+++ client/src/App.js
@@ -16,66 +16,66 @@ import Web3 from 'web3';
 const hist = createBrowserHistory();
 
 class App extends Component {
-    constructor () {
+    constructor() {
         super();
         this.state = {
             'account': null,
             'rideManager': null,
             'loading': true,
-            'web3': null,
+            // 'web3': null,
         }
     }
 
     async componentWillMount() {
-        await this.loadWeb3()
-        await this.loadBlockChain()
+        // await this.loadWeb3()
+        // await this.loadBlockChain()
     }
 
-    async loadWeb3() {
-        if (window.ethereum) {
-            window.web3 = new Web3(window.ethereum);
-            await window.ethereum.enable();
-        }
-        else if (window.web3) {
-            window.web3 = new Web3(window.web3.currentProvider);
-        }
-        else {
-            window.alert('Non-Ethereum browser detected. You should consider trying MetaMask!');
-        }
-    }
+    // async loadWeb3() {
+    //     if (window.ethereum) {
+    //         window.web3 = new Web3(window.ethereum);
+    //         await window.ethereum.enable();
+    //     }
+    //     else if (window.web3) {
+    //         window.web3 = new Web3(window.web3.currentProvider);
+    //     }
+    //     else {
+    //         window.alert('Non-Ethereum browser detected. You should consider trying MetaMask!');
+    //     }
+    // }
 
     handleInputChange = (e) => {
         this.setState({
-            [ e.target.id ]: e.target.value,
+            [e.target.id]: e.target.value,
         })
     }
 
-    async loadBlockChain() {
-        const web3 = window.web3
-        const accounts = await web3.eth.getAccounts();
-        console.log(accounts);
-        this.setState({ 'account': accounts[ 0 ] });
-        const networkId = await web3.eth.net.getId();
-        const networkData = RideManager.networks[ networkId ];
-        if (networkData) {
-            const rideManager = new web3.eth.Contract(RideManager.abi, networkData.address);
-            this.setState({ 'rideManager': rideManager, 'loading': false, 'web3': web3 });
-        } else {
-            window.alert('Ride Manager contract not deployed to detected network.');
-        }
-    }
+    // async loadBlockChain() {
+    //     const web3 = window.web3
+    //     const accounts = await web3.eth.getAccounts();
+    //     console.log(accounts);
+    //     this.setState({ 'account': accounts[ 0 ] });
+    //     const networkId = await web3.eth.net.getId();
+    //     const networkData = RideManager.networks[ networkId ];
+    //     if (networkData) {
+    //         const rideManager = new web3.eth.Contract(RideManager.abi, networkData.address);
+    //         this.setState({ 'rideManager': rideManager, 'loading': false, 'web3': web3 });
+    //     } else {
+    //         window.alert('Ride Manager contract not deployed to detected network.');
+    //     }
+    // }
 
     render() {
-        if (!this.state.web3) {
-            return <div>Loading Web3, accounts, and contract...</div>;
-        }
+        // if (!this.state.web3) {
+        //     return <div>Loading Web3, accounts, and contract...</div>;
+        // }
         return (
             <Router history={hist}>
                 <Switch>
                     <Route
                         path="/admin"
                         render={(props) => (
-                            <Admin rideManager={this.state.rideManager} web3={ this.state.web3 }/>
+                            <Admin rideManager={this.state.rideManager} web3={this.state.web3} />
                         )}
                     />
                     <Redirect from="/" to="/admin/dashboard" />
--- client/src/views/Maps/Maps.js
+++ client/src/views/Maps/Maps.js
@@ -4,10 +4,10 @@ import _ from 'lodash';
 //import { DirectionsRenderer } from "react-google-maps";
 import { compose, withProps, lifecycle } from "recompose";
 import {
-  withScriptjs,
-  withGoogleMap,
-  GoogleMap,
-  DirectionsRenderer,
+    withScriptjs,
+    withGoogleMap,
+    GoogleMap,
+    DirectionsRenderer,
 
 } from "react-google-maps";
 
@@ -15,197 +15,218 @@ const { InfoBox } = require("react-google-maps/lib/components/addons/InfoBox");
 const { SearchBox } = require("react-google-maps/lib/components/places/SearchBox");
 
 const Maps = compose(
-  withProps({
-    googleMapURL:
-      "https://maps.googleapis.com/maps/api/js?key=AIzaSyAUxSCFAa8dpHXlqjdMlRRvuQm1rbUUP7A&v=3.exp&libraries=geometry,drawing,places",
-    loadingElement: <div style={{ height: `100%` }} />,
-    containerElement: <div style={{ height: `1000px` }} />,
-    mapElement: <div style={{ height: `100%` }} />
-  }),
-  withScriptjs,
-  withGoogleMap,
+    withProps({
+        googleMapURL:
+            "https://maps.googleapis.com/maps/api/js?key=AIzaSyAUxSCFAa8dpHXlqjdMlRRvuQm1rbUUP7A&v=3.exp&libraries=geometry,drawing,places",
+        loadingElement: <div style={{ height: `100%` }} />,
+        containerElement: <div style={{ height: `1000px` }} />,
+        mapElement: <div style={{ height: `100%` }} />
+    }),
+    withScriptjs,
+    withGoogleMap,
 
-  lifecycle({
-    componentDidMount() {
-      const DirectionsService = new google.maps.DirectionsService();
+    lifecycle({
+        componentDidMount() {
+            const DirectionsService = new google.maps.DirectionsService();
 
-      DirectionsService.route(
-        {
-          origin: new google.maps.LatLng(19.1217707406339, 72.83944134193857),
-          destination: new google.maps.LatLng(19.17060002212831, 72.79517092313263),
-          travelMode: google.maps.TravelMode.DRIVING
+            DirectionsService.route(
+                {
+                    origin: new google.maps.LatLng(19.1217707406339, 72.83944134193857),
+                    destination: new google.maps.LatLng(19.17060002212831, 72.79517092313263),
+                    travelMode: google.maps.TravelMode.DRIVING
+                },
+
+                (result, status) => {
+                    if (status === google.maps.DirectionsStatus.OK) {
+                        this.setState({
+                            directions: result,
+                            distance: result.routes[0].legs[0].distance.text,
+                            time: result.routes[0].legs[0].duration.text,
+                        });
+                    } else {
+                        console.error(`error fetching directions ${result}`);
+                    }
+                }
+            );
         },
 
-        (result, status) => {
-          if (status === google.maps.DirectionsStatus.OK) {
+        componentWillMount() {
+            const refs = {}
+
             this.setState({
-              directions: result
+                bounds: null,
+                source: new google.maps.LatLng(19.1217707406339, 72.83944134193857),
+                destination: new google.maps.LatLng(19.17060002212831, 72.79517092313263),
+                center: {
+                    lat: 41.9, lng: -87.624
+                },
+                markers: [],
+                onMapMounted: ref => {
+                    refs.map = ref;
+                },
+                onBoundsChanged: () => {
+                    this.setState({
+                        bounds: refs.map.getBounds(),
+                        center: refs.map.getCenter(),
+                    })
+                },
+                onSourceBoxMounted: ref => {
+                    refs.sourceBox = ref;
+                },
+                onDestinationBoxMounted: ref => {
+                    refs.destinationBox = ref;
+                },
+                onSourcePlacesChanged: () => {
+                    const places = refs.sourceBox.getPlaces();
+                    const bounds = new google.maps.LatLngBounds();
+
+                    places.forEach(place => {
+                        if (place.geometry.viewport) {
+                            bounds.union(place.geometry.viewport)
+                        } else {
+                            bounds.extend(place.geometry.location)
+                        }
+                    });
+                    const nextMarkers = places.map(place => ({
+                        position: place.geometry.location,
+                    }));
+                    const nextCenter = _.get(nextMarkers, '0.position', this.state.center);
+
+
+                    let s = { lat: nextCenter.lat(), lng: nextCenter.lng() };
+                    this.setState({
+                        center: nextCenter,
+                        markers: nextMarkers,
+                        source: s
+                    });
+                    refs.map.fitBounds(bounds);
+                },
+                onDestinationPlacesChanged: () => {
+                    console.log(refs);
+                    const places = refs.destinationBox.getPlaces();
+                    const bounds = new google.maps.LatLngBounds();
+
+                    places.forEach(place => {
+                        if (place.geometry.viewport) {
+                            bounds.union(place.geometry.viewport)
+                        } else {
+                            bounds.extend(place.geometry.location)
+                        }
+                    });
+                    const nextMarkers = places.map(place => ({
+                        position: place.geometry.location,
+                    }));
+                    const nextCenter = _.get(nextMarkers, '0.position', this.state.center);
+
+                    console.log(nextCenter);
+                    let d = { lat: nextCenter.lat(), lng: nextCenter.lng() };
+                    this.state.getRoute(this.state.source, d);
+                    this.setState({
+                        center: nextCenter,
+                        markers: nextMarkers,
+                        destination: d,
+                    });
+
+                    refs.map.fitBounds(bounds);
+                },
+                getRoute: (source, destination) => {
+                    console.log('Source', source.lat, source.lng);
+                    console.log('Destination', destination.lat, destination.lng);
+                    const DirectionsService = new google.maps.DirectionsService();
+
+                    DirectionsService.route(
+                        {
+                            origin: new google.maps.LatLng(source),
+                            destination: new google.maps.LatLng(destination),
+                            travelMode: google.maps.TravelMode.DRIVING
+                        },
+                        (result, status) => {
+                            if (status === google.maps.DirectionsStatus.OK) {
+                                console.log(result);
+                                localStorage.setItem('sourceLat', source.lat);
+                                localStorage.setItem('sourceLng', source.lng);
+                                localStorage.setItem('destinationLat', destination.lat);
+                                localStorage.setItem('destinationLng', destination.lng)
+                                localStorage.setItem('distance', result.routes[0].legs[0].distance.text);
+                                localStorage.setItem('time', result.routes[0].legs[0].duration.text);
+                                this.setState({
+                                    directions: result,
+                                    distance: result.routes[0].legs[0].distance.text,
+                                    time: result.routes[0].legs[0].duration.text,
+                                });
+                            } else {
+                                console.error(`error fetching directions ${result}`);
+                            }
+                        }
+                    );
+                }
             });
-          } else {
-            console.error(`error fetching directions ${result}`);
-          }
-        }
-      );
-    },
-
-    componentWillMount() {
-      const refs = {}
-
-      this.setState({
-        bounds: null,
-        source: { lat: 19.1217707406339, lng: 72.83944134193857 },
-        destination: { lat: 19.17060002212831, lng: 72.79517092313263 },
-        center: {
-          lat: 41.9, lng: -87.624
         },
-        markers: [],
-        onMapMounted: ref => {
-          refs.map = ref;
-        },
-        onBoundsChanged: () => {
-          this.setState({
-            bounds: refs.map.getBounds(),
-            center: refs.map.getCenter(),
-          })
-        },
-        onSourceBoxMounted: ref => {
-          refs.sourceBox = ref;
-        },
-        onDestinationBoxMounted: ref => {
-          refs.destinationBox = ref;
-        },
-        onSourcePlacesChanged: () => {
-          const places = refs.sourceBox.getPlaces();
-          const bounds = new google.maps.LatLngBounds();
-
-          places.forEach(place => {
-            if (place.geometry.viewport) {
-              bounds.union(place.geometry.viewport)
-            } else {
-              bounds.extend(place.geometry.location)
-            }
-          });
-          const nextMarkers = places.map(place => ({
-            position: place.geometry.location,
-          }));
-          const nextCenter = _.get(nextMarkers, '0.position', this.state.center);
-
-
-          let s = { lat: nextCenter.lat(), lng: nextCenter.lng() };
-          this.setState({
-            center: nextCenter,
-            markers: nextMarkers,
-            source: s
-          });
-          refs.map.fitBounds(bounds);
-        },
-        onDestinationPlacesChanged: () => {
-          console.log(refs);
-          const places = refs.destinationBox.getPlaces();
-          const bounds = new google.maps.LatLngBounds();
-
-          places.forEach(place => {
-            if (place.geometry.viewport) {
-              bounds.union(place.geometry.viewport)
-            } else {
-              bounds.extend(place.geometry.location)
-            }
-          });
-          const nextMarkers = places.map(place => ({
-            position: place.geometry.location,
-          }));
-          const nextCenter = _.get(nextMarkers, '0.position', this.state.center);
-
-          console.log(nextCenter);
-          let d = { lat: nextCenter.lat(), lng: nextCenter.lng() };
-          this.state.getRoute(this.state.source, d);
-          this.setState({
-            center: nextCenter,
-            markers: nextMarkers,
-            destination: d,
-          });
-
-          refs.map.fitBounds(bounds);
-        },
-        getRoute: (source, destination) => {
-          console.log(source, destination);
-          const DirectionsService = new google.maps.DirectionsService();
-
-          DirectionsService.route(
-            {
-              origin: new google.maps.LatLng(source),
-              destination: new google.maps.LatLng(destination),
-              travelMode: google.maps.TravelMode.DRIVING
-            },
-            (result, status) => {
-              if (status === google.maps.DirectionsStatus.OK) {
-                console.log(result);
-                this.setState({
-                  directions: result,
-                  distance: result.routes[0].legs[0].distance,
-                  time: result.routes[0].legs[0].duration,
-                });
-              } else {
-                console.error(`error fetching directions ${result}`);
-              }
-            }
-          );
-        }
-      });
-    },
-  })
+    })
 )(props => (
-  <GoogleMap defaultZoom={8} ref={props.onMapMounted} defaultCenter={{ lat: 72.77587292177071, lng: 18.89286755846978 }}>
-    <SearchBox
-      ref={props.onSourceBoxMounted}
-      bounds={props.bounds}
-      controlPosition={google.maps.ControlPosition.TOP_LEFT}
-      onPlacesChanged={props.onSourcePlacesChanged}
-    >
-      <input
-        type="text"
-        placeholder="Enter Source"
-        style={{
-          boxSizing: `border-box`,
-          border: `1px solid transparent`,
-          width: `240px`,
-          height: `32px`,
-          marginTop: `27px`,
-          padding: `0 12px`,
-          borderRadius: `3px`,
-          boxShadow: `0 2px 6px rgba(0, 0, 0, 0.3)`,
-          fontSize: `14px`,
-          outline: `none`,
-          textOverflow: `ellipses`,
-        }}
-      />
-    </SearchBox>
-    <SearchBox
-      ref={props.onDestinationBoxMounted}
-      bounds={props.bounds}
-      controlPosition={google.maps.ControlPosition.TOP_LEFT}
-      onPlacesChanged={props.onDestinationPlacesChanged}
-    >
-      <input
-        type="text"
-        placeholder="Enter Destination"
-        style={{
-          boxSizing: `border-box`,
-          border: `1px solid transparent`,
-          width: `240px`,
-          height: `32px`,
-          marginTop: `27px`,
-          padding: `0 12px`,
-          borderRadius: `3px`,
-          boxShadow: `0 2px 6px rgba(0, 0, 0, 0.3)`,
-          fontSize: `14px`,
-          outline: `none`,
-          textOverflow: `ellipses`,
-        }}
-      />
-    </SearchBox>
-    {props.directions && <DirectionsRenderer directions={props.directions} />}
-  </GoogleMap>
+    <GoogleMap defaultZoom={8} ref={props.onMapMounted} defaultCenter={{ lat: 72.77587292177071, lng: 18.89286755846978 }}>
+        <SearchBox
+            ref={props.onSourceBoxMounted}
+            bounds={props.bounds}
+            controlPosition={google.maps.ControlPosition.TOP_LEFT}
+            onPlacesChanged={props.onSourcePlacesChanged}
+        >
+            <input
+                type="text"
+                placeholder="Enter Source"
+                style={{
+                    boxSizing: `border-box`,
+                    border: `1px solid transparent`,
+                    width: `240px`,
+                    height: `32px`,
+                    marginTop: `27px`,
+                    padding: `0 12px`,
+                    borderRadius: `3px`,
+                    boxShadow: `0 2px 6px rgba(0, 0, 0, 0.3)`,
+                    fontSize: `14px`,
+                    outline: `none`,
+                    textOverflow: `ellipses`,
+                }}
+            />
+        </SearchBox>
+        <SearchBox
+            ref={props.onDestinationBoxMounted}
+            bounds={props.bounds}
+            controlPosition={google.maps.ControlPosition.TOP_LEFT}
+            onPlacesChanged={props.onDestinationPlacesChanged}
+        >
+            <input
+                type="text"
+                placeholder="Enter Destination"
+                style={{
+                    boxSizing: `border-box`,
+                    border: `1px solid transparent`,
+                    width: `240px`,
+                    height: `32px`,
+                    marginTop: `27px`,
+                    padding: `0 12px`,
+                    borderRadius: `3px`,
+                    boxShadow: `0 2px 6px rgba(0, 0, 0, 0.3)`,
+                    fontSize: `14px`,
+                    outline: `none`,
+                    textOverflow: `ellipses`,
+                }}
+            />
+        </SearchBox>
+        {props.directions && <InfoBox
+            position={props.destination}
+        >
+            <div style={{ backgroundColor: 'yellow', opacity: 0.75, padding: '12px' }}>
+                <div style={{ fontSize: '16px', fontColor: '#08233B' }}>
+                    Distance: {props.distance}
+                </div>
+                <div style={{ fontSize: '16px', fontColor: '#08233B' }}>
+                    Time: {props.time}
+                </div>
+            </div>
+        </InfoBox>}
+        {props.directions && <DirectionsRenderer directions={props.directions} />}
+    </GoogleMap>
 ));
 
 export default Maps;
